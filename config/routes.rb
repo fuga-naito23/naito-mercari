cde2ed86ce5c04332b6bd638bf4c8ab98daad86a
fuga-naito23 <53104142+fuga-naito23@users.noreply.github.com>
2019-09-01 19:23:57 +0900

    gem install

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "products#index"
+  # devise_for :users
   resources :users, only: [:show] do
     member do
       get "likes", "listings", "in_progress", "completed", "purchase", "purchased"
