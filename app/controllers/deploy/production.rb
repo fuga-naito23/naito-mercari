547862e1db5ae960d0737605ea550c11c7161007
fuga-naito23 <53104142+fuga-naito23@users.noreply.github.com>
2019-09-01 16:50:20 +0900

    server

--- app/controllers/deploy/production.rb
+++ app/controllers/deploy/production.rb
@@ -1,8 +1,4 @@
-# server "db.example.com", user: "deploy", roles: %w{db}
-server "52.192.210.181", user: "ec2-user", roles: %w{app db web}
+server '52.192.210.181', user: 'ec2-user', roles: %w{app db web}
 
 set :rails_env, "production"
 set :unicorn_rack_env, "production"
-
-# role-based syntax
-# ==================
