29f69d3c979b26c58f0377f3e62d0a32dc507e10
fuga-naito23 <53104142+fuga-naito23@users.noreply.github.com>
2019-09-01 17:34:01 +0900

    delete devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: "products#index"
-  devise_for :users
   resources :users, only: [:show] do
     member do
       get "likes", "listings", "in_progress", "completed", "purchase", "purchased"
