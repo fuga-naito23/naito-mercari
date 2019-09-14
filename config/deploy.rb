ed2f57cf0f7a21c118e8abf144d11cac119465bb
fuga-naito23 <53104142+fuga-naito23@users.noreply.github.com>
2019-09-14 16:03:31 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.11.1'
 set :application, 'naito-mercari'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:fuga-naito23/chat-space.git'
+set :repo_url,  'git@github.com:fuga-naito23/naito-mercari.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
