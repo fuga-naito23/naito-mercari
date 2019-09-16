cec4dedda8987ff24f00bf9a81069573459712a2
fuga-naito23 <53104142+fuga-naito23@users.noreply.github.com>
2019-09-16 15:16:27 +0900

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "products#index"
-  # devise_for :users
   resources :users, only: [:show] do
     member do
       get "likes", "listings", "in_progress", "completed", "purchase", "purchased"
