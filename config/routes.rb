ad8a61015a9332ae236dfaa83da9f141ed4a5588
fuga-naito23 <53104142+fuga-naito23@users.noreply.github.com>
2019-09-28 19:58:00 +0900

    registration
    mail  registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations'
+   }
   root to: "products#index"
   resources :users, only: [:show] do
     member do
