f67111f598a49a9485c4ed24ddab3705be4a7c9d
fuga-naito23 <53104142+fuga-naito23@users.noreply.github.com>
2019-09-29 15:10:26 +0900

    Revert "Snsregistration"

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,24 +1,30 @@
+# frozen_string_literal: true
+
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def facebook
-    authorization
-  end
+  # You should configure your model like this:
+  # devise :omniauthable, omniauth_providers: [:twitter]
 
-  def google_oauth2
-    authorization
-  end
+  # You should also create an action method in this controller like this:
+  # def twitter
+  # end
 
-  def failure
-    redirect_to root_path
-  end
+  # More info at:
+  # https://github.com/plataformatec/devise#omniauth
 
-  private
+  # GET|POST /resource/auth/twitter
+  # def passthru
+  #   super
+  # end
 
-  def authorization
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-    else
-      render template: 'devise/registrations/new'
-    end
-  end
+  # GET|POST /users/auth/twitter/callback
+  # def failure
+  #   super
+  # end
+
+  # protected
+
+  # The path used when OmniAuth fails
+  # def after_omniauth_failure_path_for(scope)
+  #   super(scope)
+  # end
 end
