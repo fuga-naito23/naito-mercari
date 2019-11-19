efe6d163603a94ca25f474e7a8e94b0e129e0f88
fuga-naito23 <53104142+fuga-naito23@users.noreply.github.com>
2019-11-19 21:31:05 +0900

    ransack

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,2 +1,14 @@
 class CategoryController < ApplicationController
+
+  def index
+    @category = Category.all.order("id ASC").limit(13)
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    categories = @category.subtree_ids
+    @category_products = Product.where(category_id: categories).order("id DESC")
+  end
+
+
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,10 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :destroy, :update]
-  before_action :set_category, only: [:new, :edit]
+  before_action :set_category, only: [:new, :edit, :search]
 
   def index
     @products = Product.limit(20).order("id DESC")
-    
+
     redies = Category.find(1)
     redies_categorys_id = redies.subtree_ids
     @redies_categorys = Product.where(category_id: redies_categorys_id).limit(5).order("id DESC")
@@ -54,6 +54,40 @@ class ProductsController < ApplicationController
 
   end
 
+  def search
+
+    
+    # binding.pry
+    respond_to do |format|
+      format.html{
+
+
+        if params[:search] == ""
+          redirect_to root_path
+          return
+        end
+        
+        # binding.pry
+        @products = Product.search(params[:search])
+        params[:q] = { sorts: 'id DESC' }
+        set_ransack
+      }
+
+      format.json{
+
+        params[:q][:sorts] = 'id DESC' if params[:q][:sorts] = ""
+
+        params[:q][:name_cont_any] = params[:name_search].squish.split(" ") if params[:q][:name_cont_any]
+        @category = Category.find(params[:q][:category_id])
+        params[:q][:category_id] = @category.subtree_ids
+        set_ransack 
+        binding.pry
+      }
+    end
+  end
+
+
+
   private
 
   def product_params
@@ -62,7 +96,6 @@ class ProductsController < ApplicationController
 
   def delete_params
     params.require(:product).permit(:delete_ids => [])
-    # 配列で受け取る
   end
 
 
@@ -84,5 +117,14 @@ class ProductsController < ApplicationController
        format.html
        format.json
      end
+  end
+
+  def set_ransack
+    
+     ## 共通の処理
+     @q = Product.ransack(params[:q])
+     @products_ransack = @q.result(distinct: true)
+     @order = [["価格が安い順", "price ASC"], ["価格が高い順", "price DESC"], ["出品が新しい順", "created_at DESC"], ["出品が古い順", "created_at ASC"]]    
+
   end
 end
